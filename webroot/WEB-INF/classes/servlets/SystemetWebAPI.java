6b3af626348e43ee40c7ff2ad9df95a0107b6670
Rikard Fröberg <rikard@morus.se>
2018-04-08 18:06:29 +0200

    Better way to create the Writer (avoiding autocommit)

--- webroot/WEB-INF/classes/servlets/SystemetWebAPI.java
+++ webroot/WEB-INF/classes/servlets/SystemetWebAPI.java
@@ -14,28 +14,35 @@ import se.itu.systemet.storage.ProductLine;
 import se.itu.systemet.storage.ProductLineFactory;
 
 
-public class SystemetWebAPI extends HttpServlet{
+public class SystemetWebAPI extends HttpServlet {
 
   @Override
   public void init() throws ServletException {
-    System.setProperty("ProductLine", getServletContext().getInitParameter("ProductLine"));
+    System.setProperty("ProductLine",
+                       getServletContext().getInitParameter("ProductLine"));
   }
   
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException{
+    throws ServletException, IOException {
+    
     request.setCharacterEncoding(UTF_8.name());
-    response.setContentType("application/json;charset="+UTF_8.name());
-    PrintWriter out =
-      new PrintWriter(new OutputStreamWriter(response.getOutputStream(),
-                                             UTF_8), true);
-    ParameterParser paramParser = new ParameterParser(request.getQueryString().split("&"));
+    response.setContentType("application/json;charset=" + UTF_8.name());
+    response.setCharacterEncoding(UTF_8.name());
+    PrintWriter out = response.getWriter();
+
+    ParameterParser paramParser =
+      new ParameterParser(request.getQueryString().split("&"));
+
     Predicate<Product> filter = paramParser.filter();
+
     ProductLine productLine = ProductLineFactory.getProductLine();
+
     List<Product> products = productLine.getProductsFilteredBy(filter);
     
     Formatter formatter = FormatterFactory.getFormatter();
     String json = formatter.format(products);
+
     StringBuilder sb = new StringBuilder(json);
     out.println(sb.toString());
     out.close();
